a5553a554b93ecb121dcd4bcdd6ca4d501c91cde
Ekaterina Volynkina <katerina.vakutina@gmail.com>
2025-04-02 18:13:34 +0300

    Создала проверки PUT, PATCH, DELETE, CREATE, дописала везде шаги с allure

--- tests/test_get_resource_data.py
+++ tests/test_get_resource_data.py
@@ -1,3 +1,4 @@
+import allure
 import httpx
 from jsonschema import validate
 from core.contracts import RESOURCE_DATA_SCHEMA
@@ -8,34 +9,49 @@ SINGLE_RESOURCE = 'api/unknown/2'
 NOT_FOUND_RESOURCE = 'api/unknown/23'
 COLOR_START = '#'
 
+@allure.suite('RESOURCE')
+@allure.title('Проверка списка resource')
 def test_list_resource():
     response = httpx.get(BASE_URL + LIST_RESOURCE)
-    assert  response.status_code == 200
-    data = response.json()['data']
+    with allure.step('Проверка статуса'):
+        assert  response.status_code == 200
+        data = response.json()['data']
 
     for item in data:
         validate(item, RESOURCE_DATA_SCHEMA)
-        assert len(str(item['year'])) == 4
-        assert str(item['year']).isdigit() == True
-        assert len(item['color']) == 7
-        assert item['color'].startswith(COLOR_START)
-        assert item['color'][1:].isalnum() == True
-        assert len(str(item['pantone_value'])) == 7
-        assert '-' in item['pantone_value']
-        assert item['pantone_value'].isalpha() == False
-
+        with allure.step('Проверяем количество символов в year'):
+            assert len(str(item['year'])) == 4
+        with allure.step('Проверяем все ли цифры в year'):
+            assert str(item['year']).isdigit() == True
+        with allure.step('Проверяем количество символов в color'):
+            assert len(item['color']) == 7
+        with allure.step('Проверяем, что color начинается с "#"'):
+            assert item['color'].startswith(COLOR_START)
+        with allure.step('Проверяем количество символов в pantone_value'):
+            assert len(str(item['pantone_value'])) == 7
+        with allure.step('Проверяем количество символов в pantone_value'):
+            assert '-' in item['pantone_value']
+@allure.title('Проверка одного resource')
 def test_single_resource():
     response = httpx.get(BASE_URL + SINGLE_RESOURCE)
-    assert response.status_code == 200
+    with allure.step('Проверка статуса'):
+        assert response.status_code == 200
     data = response.json()['data']
-    assert len(str(data['year'])) == 4
-    assert str(data['year']).isdigit() == True
-    assert len(data['color']) == 7
-    assert data['color'].startswith(COLOR_START)
-    assert len(str(data['pantone_value'])) == 7
-    assert '-' in data['pantone_value']
+    with allure.step('Проверяем количество символов в year'):
+        assert len(str(data['year'])) == 4
+    with allure.step('Проверяем все ли цифры в year'):
+        assert str(data['year']).isdigit() == True
+    with allure.step('Проверяем количество символов в color'):
+        assert len(data['color']) == 7
+    with allure.step('Проверяем, что color начинается с "#"'):
+        assert data['color'].startswith(COLOR_START)
+    with allure.step('Проверяем количество символов в pantone_value'):
+        assert len(str(data['pantone_value'])) == 7
+    with allure.step('Проверяем количество символов в pantone_value'):
+        assert '-' in data['pantone_value']
 
-
+@allure.title('Удаление ресурса')
 def test_resource_not_found():
     response = httpx.get(BASE_URL+ NOT_FOUND_RESOURCE)
-    assert response.status_code == 404
+    with allure.step('Проверка статуса'):
+        assert response.status_code == 404
--- tests/test_get_user_data.py
+++ tests/test_get_user_data.py
@@ -1,3 +1,4 @@
+import allure
 import httpx
 from jsonschema import validate
 from core.contracts import USER_DATA_SCHEMA
@@ -10,23 +11,34 @@ NOT_FOUND_USER = 'api/users/23'
 EMAIL_ENDS = '@reqres.in'
 AVATAR_ENDS = '-image.jpg'
 
+@allure.suite('Проверка запроса данных пользователя')
+@allure.title('Проверка списка пользователей')
 def test_list_users():
     response = httpx.get(BASE_URL + LIST_USERS)
-    assert  response.status_code == 200
+    with allure.step('Проверка статуса'):
+        assert  response.status_code == 200
     data = response.json()['data']
 
     for item in data:
         validate(item, USER_DATA_SCHEMA)
-        assert item['email'].endswith(EMAIL_ENDS)
-        assert item['avatar'].endswith(str(item['id']) + AVATAR_ENDS)
+        with allure.step('Проверяем окончание EMAIL'):
+            assert item['email'].endswith(EMAIL_ENDS)
+        with allure.step('Проверяем есть ли в avatar id пользователя'):
+            assert item['avatar'].endswith(str(item['id']) + AVATAR_ENDS)
 
+@allure.title('Проверка одного пользователя')
 def test_single_user():
     response = httpx.get(BASE_URL + SINGLE_USER)
-    assert response.status_code == 200
+    with allure.step('Проверка статуса'):
+        assert response.status_code == 200
     data = response.json()['data']
-    assert data['email'].endswith(EMAIL_ENDS)
-    assert data['avatar'].endswith(str(data['id']) + AVATAR_ENDS)
+    with allure.step('Проверяем окончание EMAIL'):
+        assert data['email'].endswith(EMAIL_ENDS)
+    with allure.step('Проверяем есть ли в avatar id пользователя'):
+        assert data['avatar'].endswith(str(data['id']) + AVATAR_ENDS)
 
+@allure.title('Удаление пользователя')
 def test_user_not_found():
     response = httpx.get(BASE_URL+ NOT_FOUND_USER)
-    assert response.status_code == 404
+    with allure.step('Проверка статуса'):
+        assert response.status_code == 404
